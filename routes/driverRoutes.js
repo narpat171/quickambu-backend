4c27673d427492932558852ba04f2e0fcf6ab8cd
narpat171 <ns7976144@gmail.com>
2026-06-19 09:00:24 +0530

    Add files via upload

--- routes/driverRoutes.js
+++ routes/driverRoutes.js
@@ -30,7 +30,7 @@ const protect = async (req, res, next) => {
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
             token = req.headers.authorization.split(' ')[1];
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            const decoded = jwt.verify(token, "NarpatAmbu12345");
             req.driverId = decoded.id; 
             next();
         } catch (error) {
